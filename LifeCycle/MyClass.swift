f6e19d18dc9f11d45416431591db5220b7766e90
Eduardo <eduardodiasdini@gmail.com>
2022-06-09 11:59:58 -0300

    Crete MyClass() and test_zero()

---
+++ LifeCycle/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  LifeCycle
+//
+//  Created by Eduardo Dini on 09/06/22.
+//
+
+import Foundation
+
+class MyClass {
+    func methodOne() {
+        print(">> methodOne")
+    }
+
+    func methodTwo() {
+        print(">> methodTwo")
+    }
+}
