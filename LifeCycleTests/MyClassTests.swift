68b0fee55f066722c8e9f3b55c4f160ce9b05668
Eduardo <eduardodiasdini@gmail.com>
2022-06-09 12:36:17 -0300

    Observing object life cycles

--- LifeCycleTests/MyClassTests.swift
+++ LifeCycleTests/MyClassTests.swift
@@ -14,6 +14,7 @@ class MyClassTests: XCTestCase {
         // sut -> system under test
         let sut = MyClass()
         sut.methodOne()
+        XCTFail("failed test ")
     }
 
     func test_methodTwo() {
